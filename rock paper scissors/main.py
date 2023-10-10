d9e5a7229f20ca813d869b73c1ebf28343450c1a
Groinkb <benjamin@mossu.ch>
2023-10-10 18:18:09 +0200

    rock scissor done

--- rock paper scissors/main.py
+++ rock paper scissors/main.py
@@ -3,8 +3,33 @@ import random
 options = ("rock", "paper", "scissors")
 player = None
 computer =random.choice(options)
+running = True
 
-player = input("Enter a choice(rock, paper, scissors): ")
+while running :
+    while player not in options :
+        player = input("Enter a choice(rock, paper, scissors): ")
 
-print(f"Player: {player}")
-print(f"Computer: {computer}")
+    print(f"Player: {player}")
+    print(f"Computer: {computer}")
+
+    if player == computer:
+        print("it's a tie!")
+
+    elif player == "rock" and computer =="scissors":
+        print ("You win")
+
+    elif player == "paper" and computer == "rock":
+        print("You win !")
+
+    elif player == "scissors" and computer == "paper":
+        print("you win!")
+
+    else:
+        print("You lose !")
+
+    play_again = input("Play again ? (y/n):").lower()
+    if not play_again == "y":
+
+        running = False
+
+print ("Thanks for playing")
